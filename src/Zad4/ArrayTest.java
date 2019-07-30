1b9272f471233ed60e6d840532606b704075ff3b
Tomek <kemot15@tlen.pl>
2019-07-30 12:56:43 +0200

    Zad 4 ver 2

--- src/Zad4/ArrayTest.java
+++ src/Zad4/ArrayTest.java
@@ -5,10 +5,8 @@ public class ArrayTest {
         int[] tab1 = {1,2,3,4,5};
         int[] tab2 = {1,2,3,4,5};
         int[][] tab3 = {{1,2},{1,2}};
-        int[][] tab4 = {{1,2},{2,1}};
+        int[][] tab4 = {{1,2,3},{1,2}};
         ArrayComparator.showInfo(ArrayComparator.compare(tab1, tab2));
         ArrayComparator.showInfo(ArrayComparator.compare(tab3, tab4));
-
-
     }
 }
