affddb38050a069afec2f62a1903d7b6a30d7992
Tomek <kemot15@tlen.pl>
2019-07-31 16:07:11 +0200

    Zad 4 ver 3

--- src/Zad4/ArrayComparator.java
+++ src/Zad4/ArrayComparator.java
@@ -1,31 +1,73 @@
 package Zad4;
 
+import java.util.Arrays;
+
 public class ArrayComparator {
     private static boolean result = true;
     public static boolean compare (int[] array1, int[] array2){
-        if (array1.length == array2.length && array1.length > 0 && array2.length > 0){
-            for (int i = 0; i < array1.length; i++){
-                if (array1[i] != array2[i]){
-                    result = false;
-                    return result;
+        if (isNotNull(array1) && isNotNull(array2)) {
+            if (lenghtCheck(array1, array2)){
+                for (int i = 0; i < array1.length; i++){
+                    if (array1[i] != array2[i]) {
+                        result = false;
+                        return result;
+                    }
                 }
+            }else{
+                result = false;
+                return result;
             }
         }else{
             result = false;
             return result;
         }
+
         return true;
     }
 
     public static boolean compare (int[][] array1, int[][] array2) {
         int var = 0;
-        for (int i = 0; i < array1.length; i++){
-            compare(array1[i], array2[i]);
+        if (isNotNull(array1) && isNotNull(array2)) {
+            if (lenghtCheck(array1, array2)) {
+                for (int i = 0; i < array1.length; i++){
+                    compare(array1[i], array2[i]);
+                }
+            } else {
+                result = false;
+                return result;
+            }
+        }else{
+            result = false;
+            return result;
         }
             return result;
     }
 
     public static void showInfo (boolean result){
         System.out.println("Tablice sa " + (result?"rowne":"rozne albo puste"));
+    }
+
+    public static boolean isNotNull (int[] array){
+        if (Arrays.toString(array) != "null")
+            return true;
+        else return false;
+    }
+
+    public static boolean isNotNull (int[][] array){
+        if (Arrays.toString(array) != "null")
+            return true;
+        else return false;
+    }
+
+    public static boolean lenghtCheck (int[] tab1, int[] tab2){
+        if (tab1.length == tab2.length)
+            return true;
+        else return false;
+    }
+
+    public static boolean lenghtCheck (int[][] tab1, int[][] tab2){
+        if (tab1.length == tab2.length)
+            return true;
+        else return false;
     }
 }
