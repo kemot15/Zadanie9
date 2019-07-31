8fb8cfdbd497c02ff8c633c7fd2d2303357ade7d
Tomek <kemot15@tlen.pl>
2019-07-31 15:02:05 +0200

    Zad 4 ver 3

--- src/Zad4/ArrayTest.java
+++ src/Zad4/ArrayTest.java
@@ -1,11 +1,13 @@
 package Zad4;
 
+import java.util.Arrays;
+
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] tab1 = {1,2,3,4,5};
+        int[] tab1 = null;//{1,2,3,4,5};//null;
         int[] tab2 = {1,2,3,4,5};
-        int[][] tab3 = {{1,2},{1,2}};
-        int[][] tab4 = {{1,2,3},{1,2}};
+        int[][] tab3 = {{1,2},{1,2},{1,2}};
+        int[][] tab4 = {{1,2},{1,2},{1,2}};//{{1,3},{1,2}};
         ArrayComparator.showInfo(ArrayComparator.compare(tab1, tab2));
         ArrayComparator.showInfo(ArrayComparator.compare(tab3, tab4));
     }
