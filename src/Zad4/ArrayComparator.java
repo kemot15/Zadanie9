f15f11978bd44efb394e55dedcd5e2f379d5c601
Tomek <kemot15@tlen.pl>
2019-08-01 12:21:46 +0200

    Zad 4 ver 6

--- src/Zad4/ArrayComparator.java
+++ src/Zad4/ArrayComparator.java
@@ -4,32 +4,24 @@ import java.util.Arrays;
 
 public class ArrayComparator {
     public static boolean compare (int[] array1, int[] array2){
-        if (isNotNull(array1) && isNotNull(array2)) {
-            if (lenghtCheck(array1, array2)){
+        if (array1 != null && array2 != null && lenghtCheck(array1, array2)) {
                 for (int i = 0; i < array1.length; i++){
                     if (array1[i] != array2[i]) {
                         return false;
                     }
                 }
-            }else{
-                return false;
-            }
         }else{
             return false;
         }
-
         return true;
     }
 
     public static boolean compare (int[][] array1, int[][] array2) {
-        if (isNotNull(array1) && isNotNull(array2)) {
-            if (lenghtCheck(array1, array2)) {
+        if (array1!=null && array2 != null && lenghtCheck(array1, array2)) {
                 for (int i = 0; i < array1.length; i++){
-                    return compare(array1[i], array2[i]);
+                    if (!compare(array1[i], array2[i]))
+                        return false;
                 }
-            } else {
-                return false;
-            }
         }else{
             return false;
         }
@@ -38,18 +30,6 @@ public class ArrayComparator {
 
     public static void showInfo (boolean result){
         System.out.println("Tablice sa " + (result?"rowne":"rozne albo puste"));
-    }
-
-    public static boolean isNotNull (int[] array){
-        if (Arrays.toString(array) != "null")
-            return true;
-        else return false;
-    }
-
-    public static boolean isNotNull (int[][] array){
-        if (Arrays.toString(array) != "null")
-            return true;
-        else return false;
     }
 
     public static boolean lenghtCheck (int[] tab1, int[] tab2){
--- src/Zad4/ArrayTest.java
+++ src/Zad4/ArrayTest.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] tab1 = {1,2,3,4,5};//null;
+        int[] tab1 =  {1,2,3,4,5};//null;// {1,2,3,4,5};//
         int[] tab2 = {1,2,3,4,5};
         int[][] tab3 = {{1,2},{1,2},{1,2}};
-        int[][] tab4 = {{1,3},{1,2}};
+        int[][] tab4 = {{1,2},{1,2}};//null;
         ArrayComparator.showInfo(ArrayComparator.compare(tab1, tab2));
         ArrayComparator.showInfo(ArrayComparator.compare(tab3, tab4));
     }
